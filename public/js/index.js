f627765119116dc358c9fcf57fcb329a0e00fb84
Raphael <rapssail@hotmail.fr>
2019-01-15 17:46:41 +0100

    Auto-scroll to bottom if user is close to it

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
-
 let socket = io();
+
+function scrollToBottom () {
+    // Selectors
+    const messages = jQuery('#messages');
+    const newMessage = messages.children('li:last-child');
+    // Heights
+    const clientHeight = messages.prop('clientHeight');
+    const scrollTop = messages.prop('scrollTop');
+    const scrollHeight = messages.prop('scrollHeight');
+    const newMessageHeight = newMessage.innerHeight();
+    const lastMessageHeight = newMessage.prev().innerHeight();
+    console.log('inside scroll');
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+
+}
 
 socket.on('connect', function() {
     console.log('Connected to server');
@@ -20,7 +36,7 @@ socket.on('newMessage', function(message) {
     });
 
     jQuery('#messages').append(html);
-
+    scrollToBottom();
     // let li = jQuery('<li></li>');
     // li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
